14627b7b79fcb8038f60000c4ac7587f2842d659
Simon Forsberg <simon.co.forsberg@gmail.com>
2025-09-25 09:42:16 +0200

    Added getZone

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -6,4 +6,17 @@ public class Main {
     public static void main(String[] args) {
         ElpriserAPI elpriserAPI = new ElpriserAPI();
     }
+
+    private static Prisklass getZone() {
+        Prisklass zone = null;
+        while (zone == null) {
+            String zoneInput = System.console().readLine("Ange elområde (SE1, SE2, SE3, SE4): ").trim().toUpperCase();
+            try {
+                zone = Prisklass.valueOf(zoneInput);
+            } catch (IllegalArgumentException e) {
+                System.err.print("Ogiltigt värde, försök igen: ");
+            }
+        }
+        return zone;
+    }
 }
